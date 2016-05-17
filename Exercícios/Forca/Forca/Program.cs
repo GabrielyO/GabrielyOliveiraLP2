da74a02b7e7742c3ad2298c34685f6e1add88f19
Gabriely Oliveira <gaby.o.nascimento@gmail.com>
2016-05-17 14:47:08 -0300

    Remoção e adição de arquivos Forca

---
+++ Exercícios/Forca/Forca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra, letra;
+            char[] mostrar = new char[20];
+            int j = 0;
+
+            palavra = Console.ReadLine();
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                mostrar[i] = '-';
+                Console.Write(" " + mostrar[i] + " ");                
+            }
+                
+        }
+    }
+}
