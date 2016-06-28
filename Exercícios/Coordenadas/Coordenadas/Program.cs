dd496b610150bd65aa82cb53a1646220dcbce164
GabrielyO <gaby.o.nascimento@hotmail>
2016-06-28 15:09:41 -0300

    ew

--- Exercícios/Coordenadas/Coordenadas/Program.cs
+++ Exercícios/Coordenadas/Coordenadas/Program.cs
@@ -10,7 +10,7 @@ namespace Coordenadas
     {
         static void Main(string[] args)
         {
-            double sx = 0, sy = 0, x, y, xm, ym;
+            double sx = 0, sy = 0, x, y, xm, ym , dp, sd = 0;
 
             int N = int.Parse(Console.ReadLine());
 
@@ -24,14 +24,18 @@ namespace Coordenadas
                 pontos.Add(p);                
                 Console.Clear();
             }
-            for (int i = 0; i < pontos.Count; i++)
+            for (int i = 0; i < N - 1; i++)
             {
                 sx += pontos[i].x;
                 sy += pontos[i].y;
+                dp = Math.Sqrt(Math.Pow(pontos[i + 1].x - pontos[i].x, 2) + Math.Pow(pontos[i + 1].y - pontos[i].y, 2));
+                Console.WriteLine("distância ({0} , {1}  ,  {2} , {3}) = {4}", pontos[i].x, pontos[i].y, pontos[i + 1].x, pontos[i + 1].y, dp);
+                sd += dp;
             }
             xm = sx / N;
             ym = sy / N;
-            Console.WriteLine("O ponto médio é ({0} , {1})", xm, ym);       
+            Console.WriteLine("O ponto médio é ({0} , {1}) ", xm, ym);
+            Console.WriteLine("A soma das distâncias é = {0}", sd);
 
         }
     }
