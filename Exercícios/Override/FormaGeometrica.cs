7053bb97b36dc63696ca7ea7ddbd90ae6be98fa7
GabrielyO <gaby.o.nascimento@gmail.com>
2016-09-27 15:29:04 -0300

    Aula 27/09

---
+++ Exercícios/Override/FormaGeometrica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public abstract class FormaGeometrica
+    {
+        public abstract double Area();
+        public abstract double Perimetro();
+    }
+}
